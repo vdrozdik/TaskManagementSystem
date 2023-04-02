fbbd96bc9b327ad118ad71694717dec5a75a9749
vdrozdik <drozdikv90@gmail.com>
2023-04-02 12:37:53 +0300

    add simple unit test

---
+++ NUnitTestProject/TestGlobalTaskCache.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using System;
+using TaskManagementSystem.BusinessLayer;
+using TaskManagementSystem.Model;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class TestGlobalTaskCache
+    {
+        [Test]
+        public void Check_AddTask()
+        {
+            GlobalTaskCache globalTaskCache = new GlobalTaskCache();
+            globalTaskCache.AddTask(new CreateTask { 
+            TaskID = 1,
+            TaskName = "test",
+            Status = 0,    
+            });
+            Assert.That(globalTaskCache.DisplayTasks().Count, Is.EqualTo(1));
+        }
+
+        [TestCase(1)]
+        public void Check_UpdateTask(int taskId)
+        {
+            GlobalTaskCache globalTaskCache = new GlobalTaskCache();
+            globalTaskCache.AddTask(new CreateTask
+            {
+                TaskID = taskId,
+                TaskName = "test",
+                Status =(StatusTask) 0,
+            });
+            globalTaskCache.UpdateTask(new UpdateTask
+            {
+                TaskID = taskId,
+                NewStatus = (StatusTask)1,
+                UpdatedBy = "TestUser"
+
+            });
+
+
+            Assert.That(globalTaskCache.DisplayTask(taskId).Status, Is.EqualTo((StatusTask)1));
+            Assert.That(globalTaskCache.DisplayTask(taskId).UpdatedBy, Is.EqualTo("TestUser"));
+        }
+        [TestCase(1, 2)]
+        public void Check_UpdateTask_TaskIdNotExist(int taskIdCreate, int taskIdUpdate)
+        {
+            GlobalTaskCache globalTaskCache = new GlobalTaskCache();
+            globalTaskCache.AddTask(new CreateTask
+            {
+                TaskID = taskIdCreate,
+                TaskName = "test",
+                Status = (StatusTask)0,
+            });
+            globalTaskCache.UpdateTask(new UpdateTask
+            {
+                TaskID = taskIdUpdate,
+                NewStatus = (StatusTask)1,
+                UpdatedBy = "TestUser"
+
+            });
+        }
+
+        [TestCase(1, 2)]
+        public void Check_DisplayTask_TaskIdNotExist(int taskIdCreate, int taskIdDisplay)
+        {
+            GlobalTaskCache globalTaskCache = new GlobalTaskCache();
+            globalTaskCache.AddTask(new CreateTask
+            {
+                TaskID = taskIdCreate,
+                TaskName = "test",
+                Status = (StatusTask)0,
+            });
+
+            Assert.That(globalTaskCache.DisplayTask(taskIdDisplay), Is.EqualTo(null));
+        }
+    }
+}
---
+++ NUnitTestProject/TestTaskManagementController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementSystem.BusinessLayer;
+using TaskManagementSystem.Controllers;
+using TaskManagementSystem.Model;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class TestTaskManagementController
+    {
+        private Mock<ILogger<TaskManagementController>> mockLogger;
+        private Mock<IServiceBusHandler> mockServiceBusHundler;
+        private Mock<IGlobalTaskCache> mockGlobalTaskCache;
+        [SetUp]
+        public void Setup()
+        {
+            mockLogger = new Mock<ILogger<TaskManagementController>>();
+            mockServiceBusHundler = new Mock<IServiceBusHandler>();
+            mockGlobalTaskCache = new Mock<IGlobalTaskCache>();
+            mockGlobalTaskCache.Setup(t => t.DisplayTasks()).Returns(new List<CreateTask>() {
+                 new CreateTask{ TaskID = 1, Status = StatusTask.NotStarted, AssignedTo = "testUser_1", TaskName = "test_1"},
+                 new CreateTask{ TaskID = 2, Status = StatusTask.InProgress, AssignedTo = "testUser_2", TaskName = "test_2"}
+            });
+        }
+
+
+        [Test]
+        public void ControllerDisplayTaskTest()
+        {
+            var controller = new TaskManagementController(mockLogger.Object, mockServiceBusHundler.Object, mockGlobalTaskCache.Object);
+
+            var checkDisplayTask = (OkObjectResult)controller.Display();
+            var result = checkDisplayTask.Value as List<CreateTask>;
+             Assert.That(result.Count, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public async Task ControllerUpdateTask_TaskIdNotExist_Test()
+        {
+            var controller = new TaskManagementController(mockLogger.Object, mockServiceBusHundler.Object, mockGlobalTaskCache.Object);
+
+            var checkUpdateTask = await controller.Update(new UpdateTask { TaskID = 1, NewStatus = StatusTask.Completed});
+            Assert.IsInstanceOf<BadRequestObjectResult>(checkUpdateTask);
+        }
+    }
+}
